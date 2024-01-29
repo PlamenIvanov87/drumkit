2ee80d276dc4b36116fd555dfd3b1c6462cb5e94
Plamen Ivanov <random@abv.bf>
2024-01-29 15:57:32 +0200

    next add keyboars listener

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector("button").addEventListener("click",function(){alert("goot clicked");})
+for (i=0;i>5;i++){
+document.querySelector("button")[i].addEventListener("click",function(){alert("made it");})    
+}
